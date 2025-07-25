7121899f534e62fbae642c2d6948e96188f61f0c
sergiorozada12 <sergiorozada12@gmail.com>
2025-07-25 13:08:11 +0200

    refactor of taxi data

--- src/datasets/taxi.py
+++ src/datasets/taxi.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from shapely import wkt
 import torch
+from typing import Optional
 
 from src.estimation.empirical import EmpiricalEstimator
 from src.chains.models import MarkovChainMatrix, MarkovChainTensor
@@ -19,159 +20,121 @@ class TaxiDataset:
         self.estimator = EmpiricalEstimator()
 
     def _get_filename(self):
-        return f"taxi_{self.cfg.generation_type}.npy"
+        return f"taxi_data_preproc.npy"
 
     def _get_full_path(self):
         return os.path.join(self.cfg.data_dir, self._get_filename())
 
     def get_data(self, use_tensor: bool = False):
         if os.path.exists(self._get_full_path()):
-            return self._load_data(use_tensor)
+            self._load_data()
         else:
             self._generate_chain()
-            return self._generate_data(use_tensor)
+            self._generate_data()
+        return self._get_data(use_tensor)
 
     def _generate_chain(self):
         self._load_raw_data()
         self._preprocess_zones()
         self._preprocess_trips()
         self._filter_manhattan_trips()
-        self._build_transition_tensors()
-        print("Dataset preprocessed. Starting chain generation...")
 
-    def _generate_data(self, use_tensor: bool):
-        dims = torch.tensor(self._P_true_ten.shape[:2])
-        I = torch.prod(dims).item()
-        self.I = I
-        self.Is = dims
+        _, R_ten, R_vec, Q_ten, Q_mat, P_ten, P_mat = self._build_transition_tensors(df_raw=self.df_trips_manhattan)
+        self._R_true_ten = R_ten
+        self._R_true_vec = R_vec
+        self._Q_true_ten = Q_ten
+        self._Q_true_mat = Q_mat
+        self._P_true_ten = P_ten
+        self._P_true_mat = P_mat
 
-        if self.cfg.generation_type == "matrix":
-            mc = MarkovChainMatrix(P=self._P_true_mat, R=self._R_true_vec, Q=self._Q_true_mat)
-            trajectories = mc.simulate(
-                num_steps=self.cfg.length,
-                num_trajectories=self.cfg.trials,
-                burn_in=self.cfg.burn_in,
-            )
-            estimates = self.estimator.estimate_matrix_batch(trajectories, I)
-            P_emp_mats, _ = zip(*estimates)
+    def _generate_data(self):
+        self.R_ten_list, self.R_vec_list = [], []
+        self.Q_ten_list, self.Q_mat_list = [], []
+        self.P_ten_list, self.P_mat_list = [], []
+        for trial in range(self.cfg.trials):
+            R_ten_trial, R_vec_trial = {}, {} 
+            Q_ten_trial, Q_mat_trial = {}, {}
+            P_ten_trial, P_mat_trial = {}, {}
+            df = self.df_trips_manhattan.copy(deep=True)
+            for n in self.cfg.n_samples_all[::-1]:
+                df, R_ten, R_vec, Q_ten, Q_mat, P_ten, P_mat = self._build_transition_tensors(df_raw=df, n=n)
+                R_ten_trial[n], R_vec_trial[n] = R_ten, R_vec 
+                Q_ten_trial[n], Q_mat_trial[n] = Q_ten, Q_mat 
+                P_ten_trial[n], P_mat_trial[n] = P_ten, P_mat 
+            self.R_ten_list.append(R_ten_trial), self.R_vec_list.append(R_vec_trial)
+            self.Q_ten_list.append(Q_ten_trial), self.Q_mat_list.append(Q_mat_trial)
+            self.P_ten_list.append(P_ten_trial), self.P_mat_list.append(P_mat_trial)
+        self.save_data()
+
+    def _get_data(self, use_tensor: bool):
+        if not use_tensor:
+            mc_true = [MarkovChainMatrix(
+                P=self._P_true_mat,
+                R=self._R_true_vec,
+                Q=self._Q_true_mat) for _ in range(self.cfg.trials)]
+
+            mc_emp = [MarkovChainMatrix(
+                P=self.P_mat_list[trial][self.cfg.n_samples],
+                R=self.R_vec_list[trial][self.cfg.n_samples],
+                Q=self.Q_mat_list[trial][self.cfg.n_samples]) for trial in range(self.cfg.trials)]
+
         else:
-            mc = MarkovChainTensor(P=self._P_true_ten, R=self._R_true_ten, Q=self._Q_true_ten)
-            trajectories = mc.simulate(
-                num_steps=self.cfg.length,
-                num_trajectories=self.cfg.trials,
-                burn_in=self.cfg.burn_in,
-            )
-            estimates = self.estimator.estimate_tensor_batch(trajectories, dims)
-            P_emp_tensors, _ = zip(*estimates)
-            P_emp_mats = [ten_to_mat(P, I) for P in P_emp_tensors]
-        self._P_emp_list_mat = list(P_emp_mats)
-
-        mc_true = [MarkovChainMatrix(
-            P=self._P_true_mat,
-            R=self._R_true_vec,
-            Q=self._Q_true_mat) for _ in range(self.cfg.trials)]
-        mc_emp = [MarkovChainMatrix(P) for P in P_emp_mats]
-
-        trajectories_true = [
-            mc.simulate(
-                num_steps=self.cfg.length,
-                num_trajectories=1,
-                burn_in=self.cfg.burn_in,
-            )[0]
-            for mc in mc_true
-        ]
-        trajectories_emp = [
-            mc.simulate(
-                num_steps=self.cfg.length,
-                num_trajectories=1,
-                burn_in=self.cfg.burn_in,
-            )[0]
-            for mc in mc_emp
-        ]
-
-        if use_tensor:
-            trajectories_true = [chain_mat_to_ten(traj, dims) for traj in trajectories_true]
-            trajectories_emp = [chain_mat_to_ten(traj, dims) for traj in trajectories_emp]
-
-            P_emp_tensors = [mat_to_ten(P, dims) for P in P_emp_mats]
             mc_true = [MarkovChainTensor(
                 P=self._P_true_ten,
                 R=self._R_true_ten,
                 Q=self._Q_true_ten) for _ in range(self.cfg.trials)]
-            mc_emp = [MarkovChainTensor(P) for P in P_emp_tensors]
-            trajectories_true = torch.stack([
-                torch.stack([torch.tensor(x, dtype=torch.long) for x in traj])
-                for traj in trajectories_true
-            ])
-            trajectories_emp = torch.stack([
-                torch.stack([torch.tensor(x, dtype=torch.long) for x in traj])
-                for traj in trajectories_emp
-            ])
-        else:
-            trajectories_true = torch.tensor(trajectories_true, dtype=torch.long)
-            trajectories_emp = torch.tensor(trajectories_emp, dtype=torch.long)
 
-        self.mc_true = mc_true
-        self.mc_emp = mc_emp
-        self.trajectories_true = trajectories_true
-        self.trajectories_emp = trajectories_emp
+            mc_emp = [MarkovChainTensor(
+                P=self.P_ten_list[trial][self.cfg.n_samples],
+                R=self.R_ten_list[trial][self.cfg.n_samples],
+                Q=self.Q_ten_list[trial][self.cfg.n_samples]) for trial in range(self.cfg.trials)]
 
-        self.save_data()
-        return trajectories_true, trajectories_emp, mc_true, mc_emp
+        traj_true = [None for _ in range(self.cfg.trials)]
+        traj_emp = [None for _ in range(self.cfg.trials)]
+
+        return traj_true, traj_emp, mc_true, mc_emp
 
     def save_data(self):
-        assert self._P_true_mat is not None and self._P_emp_list_mat is not None
+        assert self._P_true_mat is not None and self.P_mat_list is not None
 
         data_path = self._get_full_path()
         os.makedirs(os.path.dirname(data_path), exist_ok=True)
         np.save(
             data_path,
             {
-                "P_true": self._P_true_mat.numpy(),
-                "P_emp": [P.numpy() for P in self._P_emp_list_mat],
-                "trajectories_true": self.trajectories_true.numpy(),
-                "trajectories_emp": self.trajectories_emp.numpy(),
-                "dims": self.Is,
+                "P_mat_true": self._P_true_mat.numpy(),
+                "P_ten_true": self._P_true_ten.numpy(),
+                "R_vec_true": self._R_true_vec.numpy(),
+                "R_ten_true": self._R_true_ten.numpy(),
+                "Q_mat_true": self._Q_true_mat.numpy(),
+                "Q_ten_true": self._Q_true_ten.numpy(),
+                "P_mat_emp": [{k: v.numpy() for k, v in d.items()} for d in self.P_mat_list],
+                "P_ten_emp": [{k: v.numpy() for k, v in d.items()} for d in self.P_ten_list],
+                "R_vec_emp": [{k: v.numpy() for k, v in d.items()} for d in self.R_vec_list],
+                "R_ten_emp": [{k: v.numpy() for k, v in d.items()} for d in self.R_ten_list],
+                "Q_mat_emp": [{k: v.numpy() for k, v in d.items()} for d in self.Q_mat_list],
+                "Q_ten_emp": [{k: v.numpy() for k, v in d.items()} for d in self.Q_ten_list],
             },
         )
 
-    def _load_data(self, use_tensor: bool):
+    def _load_data(self):
         data_path = self._get_full_path()
         data = np.load(data_path, allow_pickle=True).item()
 
-        P_true = torch.tensor(data["P_true"])
-        P_emp_list = [torch.tensor(P) for P in data["P_emp"]]
-        trajectories_true = data["trajectories_true"]
-        trajectories_emp = data["trajectories_emp"]
-        dims = torch.tensor(data.get("dims", [P_true.shape[0]]))
-        I = torch.prod(dims).item()
+        self._P_true_mat = torch.tensor(data["P_mat_true"])
+        self._P_true_ten = torch.tensor(data["P_ten_true"])
+        self._R_true_vec = torch.tensor(data["R_vec_true"])
+        self._R_true_ten = torch.tensor(data["R_ten_true"])
+        self._Q_true_mat = torch.tensor(data["Q_mat_true"])
+        self._Q_true_ten = torch.tensor(data["Q_ten_true"])
+        self.P_mat_list = [{k: torch.from_numpy(v).float() for k, v in d.items()} for d in data["P_mat_emp"]]
+        self.P_ten_list = [{k: torch.from_numpy(v).float() for k, v in d.items()} for d in data["P_ten_emp"]]
+        self.R_vec_list = [{k: torch.from_numpy(v).float() for k, v in d.items()} for d in data["R_vec_emp"]]
+        self.R_ten_list = [{k: torch.from_numpy(v).float() for k, v in d.items()} for d in data["R_ten_emp"]]
+        self.Q_mat_list = [{k: torch.from_numpy(v).float() for k, v in d.items()} for d in data["Q_mat_emp"]]
+        self.Q_ten_list = [{k: torch.from_numpy(v).float() for k, v in d.items()} for d in data["Q_ten_emp"]] 
 
-        self.I = I
-        self.Is = dims
-        self.cfg.dims = dims.tolist()
-        self.cfg.trials = len(P_emp_list)
-        self.cfg.length = trajectories_true.shape[1]
-
-        mc_true = [MarkovChainMatrix(P_true) for _ in range(self.cfg.trials)]
-        mc_emp = [MarkovChainMatrix(P) for P in P_emp_list]
-
-        if use_tensor:
-            trajectories_true = torch.tensor(trajectories_true, dtype=torch.long)
-            trajectories_emp = torch.tensor(trajectories_emp, dtype=torch.long)
-            P_true_tensor = mat_to_ten(P_true, dims)
-            P_emp_tensors = [mat_to_ten(P, dims) for P in P_emp_list]
-            mc_true = [MarkovChainTensor(P_true_tensor) for _ in range(self.cfg.trials)]
-            mc_emp = [MarkovChainTensor(P) for P in P_emp_tensors]
-        else:
-            trajectories_true = torch.tensor(trajectories_true, dtype=torch.long)
-            trajectories_emp = torch.tensor(trajectories_emp, dtype=torch.long)
-
-        self.mc_true = mc_true
-        self.mc_emp = mc_emp
-        self.trajectories_true = trajectories_true
-        self.trajectories_emp = trajectories_emp
-
-        return trajectories_true, trajectories_emp, mc_true, mc_emp
+        self.Is = torch.tensor(self._P_true_ten.shape[:2])
 
     def _load_raw_data(self):
         self.df_trips = pd.read_parquet(self.cfg.trips_path)
@@ -221,9 +184,32 @@ class TaxiDataset:
 
     def _filter_manhattan_trips(self):
         df = self.df_trips
+
+        neighborhoods_from_central_park_to_south = [
+            'Alphabet City', 'Battery Park', 'Battery Park City', 'Central Park',
+            'Chinatown', 'Clinton East', 'Clinton West', 'East Chelsea', 
+            'East Village', 'Financial District North', 'Financial District South', 
+            'Flatiron', 'Hudson Sq', 'Garment District', 
+            "Governor's Island/Ellis Island/Liberty Island", 'Gramercy', 
+            'Greenwich Village North', 'Greenwich Village South', 'Kips Bay', 
+            'Lenox Hill East', 'Lenox Hill West', 'Lincoln Square East', 
+            'Lincoln Square West', 'Little Italy/NoLiTa', 'Lower East Side', 
+            'Meatpacking/West Village West', 'Midtown Center', 'Midtown East', 
+            'Midtown North', 'Midtown South', 'Murray Hill', 
+            'Penn Station/Madison Sq West', 'Seaport', 'SoHo', 
+            'Stuy Town/Peter Cooper Village', 'Sutton Place/Turtle Bay North', 
+            'Times Sq/Theatre District', 'TriBeCa/Civic Center', 
+            'Two Bridges/Seward Park', 'UN/Turtle Bay South', 'Union Sq', 
+            'Upper East Side North', 'Upper East Side South', 'Upper West Side South', 
+            'West Chelsea/Hudson Yards', 'West Village', 'World Trade Center', 
+            'Yorkville East', 'Yorkville West'
+        ]
+
         df = df[
             (df['PUborough'] == 'Manhattan') & 
-            (df['DOborough'] == 'Manhattan')
+            (df['DOborough'] == 'Manhattan') & 
+            (df['PUzone'].isin(neighborhoods_from_central_park_to_south)) & 
+            (df['DOzone'].isin(neighborhoods_from_central_park_to_south))
         ].copy()
 
         unique_pu_ids = set(df['PULocationID'].unique())
@@ -240,15 +226,18 @@ class TaxiDataset:
         df['PULocationID'] += 1
         df['DOLocationID'] += 1
 
+        self.n_periods = 6  # 24h/4
+        self.n_locations = df['PULocationID'].nunique()
         self.df_trips_manhattan = df
 
-    def _build_transition_tensors(self):
-        df = self.df_trips_manhattan
-        n_periods = 6  # 24h/4
-        n_locations = df['PULocationID'].nunique()
-        
-        R_ten = np.zeros((n_periods, n_locations))
-        Q_ten = np.zeros((n_periods, n_locations, n_periods, n_locations))
+    def _build_transition_tensors(self, df_raw, n: Optional[int]=None):
+        if n is not None:
+            df = df_raw.sample(n=n, replace=False)
+        else:
+            df = df_raw.copy(deep=True)
+
+        R_ten = np.zeros((self.n_periods, self.n_locations))
+        Q_ten = np.zeros((self.n_periods, self.n_locations, self.n_periods, self.n_locations))
 
         for _, row in df.iterrows():
             pu_period_idx = int(row['PUhour'])
@@ -270,12 +259,15 @@ class TaxiDataset:
         P_sum[P_sum == 0] = 1
         P_ten = Q_ten / P_sum
 
-        Q_mat = Q_ten.reshape(n_periods * n_locations, n_periods * n_locations)
-        P_mat = P_ten.reshape(n_periods * n_locations, n_periods * n_locations)
+        Q_mat = Q_ten.reshape(self.n_periods * self.n_locations, self.n_periods * self.n_locations)
+        P_mat = P_ten.reshape(self.n_periods * self.n_locations, self.n_periods * self.n_locations)
 
-        self._R_true_ten = torch.from_numpy(R_ten).float()
-        self._R_true_vec = torch.from_numpy(R_vec).float()
-        self._Q_true_ten = torch.from_numpy(Q_ten).float()
-        self._Q_true_mat = torch.from_numpy(Q_mat).float()
-        self._P_true_ten = torch.from_numpy(P_ten).float()
-        self._P_true_mat = torch.from_numpy(P_mat).float()
+        return (
+            df,
+            torch.from_numpy(R_ten).float(),
+            torch.from_numpy(R_vec).float(),
+            torch.from_numpy(Q_ten).float(),
+            torch.from_numpy(Q_mat).float(),
+            torch.from_numpy(P_ten).float(),
+            torch.from_numpy(P_mat).float(),
+        )
